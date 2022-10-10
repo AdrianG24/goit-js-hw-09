b9d4749db087436dde313a8ff256e213b2bbe731
AdrianG24 <adriangolodisin@gmail.com>
2022-10-10 23:17:10 +0300

    first task improved

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const background = document.body;
-
+let timerId = null;
 const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
